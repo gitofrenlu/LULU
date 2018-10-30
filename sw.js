e7b193fa0c31144c03c52e6ee0f64095fd2982f1
lu.ren <lu.ren@mljr.com>
2018-10-30 18:45:30 +0800

    test

--- sw.js
+++ sw.js
@@ -1,32 +1,56 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
 console.log('in sw.js')
 
 var cacheStorageKey = 'minimal-pwa-2'
 var cacheList=[
   '/',
-  'index.html',
-  'main.css',
-  'avatar.png'
+  '/index.html',
+  '/main.css',
+  '/avatar.png'
 ]
+
 self.addEventListener('install',e =>{
+  console.log("service worker is install");
   e.waitUntil(
     caches.open(cacheStorageKey)
     .then(cache => cache.addAll(cacheList))
-    .then(() => self.skipWaiting())
+    .then(() => {self.skipWaiting();console.log('install end')})
   )
 })
 
-self.addEventListener('fetch',function(e){
-    e.respondWith(
-      caches.match(e.request).then(function(response){
-        if(response != null){
-          return response
-        }
-        return fetch(e.request.url)
+//捕获网络请求，制定缓存策略
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+      caches.match(event.request)
+      .then(response => {
+          // Cache hit - return response
+          if (response) {
+              return response;
+          }
+          // 克隆请求。因为请求是一个“stream”，只能用一次。但我们需要用两次，一次用来缓存，一次给浏览器抓取内容，所以需要克隆
+          var fetchRequest = event.request.clone();
+          // 返回请求的内容
+          return fetch(fetchRequest).then(
+              response => {
+                  // 检查是否为有效的响应。basic表示同源响应，也就是说，这意味着，对第三方资产的请求不会添加到缓存。
+                  if (!response || response.status !== 200 || response.type !== 'basic') {
+                      return response;
+                  }
+                  // 同request，response是一个“stream”，只能用一次，但我们需要用两次，一次用来缓存一个返回给浏览器，所以需要克隆。
+                  var responseToCache = response.clone();
+                  // 缓存新请求
+                  caches.open(CACHE_NAME)
+                      .then(cache => cache.put(event.request, responseToCache));
+                  return response;
+              }
+          );
       })
-    )
-  })
+  );
+});
+
+//sw安装成功并激活   =》 用来删除过期的缓存
 self.addEventListener('activate',function(e){
+    console.log("service worker is active");
     e.waitUntil(
       //获取所有cache名称
       caches.keys().then(cacheNames => {

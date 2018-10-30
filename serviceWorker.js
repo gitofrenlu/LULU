af19c6c8196747fc80f65ea88163749267385e6e
lu.ren <lu.ren@mljr.com>
2018-10-30 16:19:17 +0800

    ver

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,6 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-var cacheStorageKey = 'minimal-pwa-1'
+importScripts("workbox-sw.js");
+
+var cacheStorageKey = 'minimal-pwa-2'
 var cacheList=[
   '/',
   'index.html',

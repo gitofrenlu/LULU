c8a7c2ea2e16b709096e1f66440954bcd129284a
lu.ren <lu.ren@mljr.com>
2018-10-31 10:51:49 +0800

    a

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
 // importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-console.log('in sw.js')
-
 var cacheStorageKey = 'minimal-pwa-3'
 var cacheList=[
   '/',

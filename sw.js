2e0380a71e3c94ecb3b36b761332186629a94b36
lu.ren <lu.ren@mljr.com>
2018-10-31 11:12:35 +0800

    v

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
 // importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-var cacheStorageKey = 'minimal-pwa-4'
+var cacheStorageKey = 'minimal-pwa-5'
 var cacheList=[
   '/',
   '/index.html',
   '/main.css',
   '/avatar.png',
-  '/icon.png'
+  '/icon.png',
+  '/newicon.png'
 ]
 
 self.addEventListener('install',e =>{
